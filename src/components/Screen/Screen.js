51f407cea4688281ac28b892790fb7283470d030
Evans Owino <e.owino@reply.com>
2017-08-14 14:40:30 +0100

    updating UI

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,25 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import INVALID_EXPRESSION from '../../errors/errors';
+import INVALID_EXPRESSION from "../../errors/errors";
 
 const Screen = props => {
   return (
     <div className="calculator-output">
-      <div className="calculator-user-input">{props.equation}</div>
-      {
-        props.error === INVALID_EXPRESSION ? 
-          <div className="calculator-error">{props.error}</div>
-          :
-          <div className="calculator-answer">{props.answer}</div>
-      }
+      <input
+        type="text"
+        className="calculator-user-input"
+        value={props.equation}
+        disabled
+      />
+      {props.error === INVALID_EXPRESSION
+        ? <div className="calculator-error">
+            {props.error}
+          </div>
+        : <input
+            type="text"
+            className="calculator-answer"
+            value={props.answer}
+            disabled
+          />}
     </div>
   );
 };
 
 Screen.proptypes = {
   equation: PropTypes.string.isRequired,
-  answer: PropTypes.string.isRequired,
+  answer: PropTypes.string.isRequired
 };
 
 export default Screen;
